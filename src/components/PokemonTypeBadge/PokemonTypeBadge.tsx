056eafe010fe111a0f970f534bd134d5d11f67ce
lArkl <g.ferro.palomino@gmail.com>
2023-07-03 09:16:14 -0500

    Add PokemonTypeBadge

---
+++ src/components/PokemonTypeBadge/PokemonTypeBadge.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+import Typography from '../Typography/Typography'
+import styles from './PokemonTypeBadge.module.scss'
+import classNames from 'classnames'
+import { capitalize } from '../../utils/strings'
+
+interface PokemonTypeBadgeProps {
+  typeName: string
+  className?: string
+  testId?: string
+}
+
+const PokemonTypeBadge: FC<PokemonTypeBadgeProps> = ({ typeName, className, testId }) => {
+  return (
+    <div
+      data-testid={testId ?? 'PokemonTypeBadge'}
+      className={classNames(styles.badge, styles[typeName], className)}
+      style={{ backgroundColor: `var(--color-${typeName})` }}
+    >
+      <Typography className={styles.title}>{capitalize(typeName)}</Typography>
+    </div>
+  )
+}
+
+export default PokemonTypeBadge
