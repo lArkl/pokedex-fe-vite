b20bef69b1dd52bb101533a72231df5f7693e79a
lArkl <g.ferro.palomino@gmail.com>
2023-12-26 14:46:26 -0500

    Update sign up form

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -6,11 +6,13 @@ import styles from './LoginPage.module.scss'
 import Loader from '../../components/Loader'
 import { Link, useNavigate } from 'react-router-dom'
 import SignInForm from '../../components/SignInForm/SignInForm'
+import useUserQuery from '../../hooks/useUserQuery'
 import Button from '../../components/Button'
 import { AppRoutes } from '../../routes/appRoutes'
 
 const Login: FC = () => {
   const navigate = useNavigate()
+  const { isFetching, data: userData, logoutUser } = useUserQuery()
 
   return (
     <div className={styles.container}>
@@ -18,18 +20,34 @@ const Login: FC = () => {
         Welcome to the Pokedex
       </Typography>
       <Loader className={styles.loader} />
-      <SignInForm
-        onSuccess={() => {
-          navigate(`/pokemons`)
-        }}
-        extraButtons={
-          <Link to={AppRoutes.PokemonList}>
-            <Button variant="secondary" className={styles.search} type="button">
-              Continue as Guest
+      {!isFetching ? (
+        userData ? (
+          <div className={styles.welcome}>
+            <Typography variant="md">Good to see you again {userData.firstname}!</Typography>
+            <Link to={AppRoutes.PokemonList}>
+              <Button variant="secondary" type="button">
+                Search Pokemons
+              </Button>
+            </Link>
+            <Button
+              variant="secondary"
+              type="button"
+              className={styles.search}
+              onClick={() => {
+                logoutUser()
+              }}
+            >
+              Logout
             </Button>
-          </Link>
-        }
-      />
+          </div>
+        ) : (
+          <SignInForm
+            onSuccess={() => {
+              navigate(`/pokemons`)
+            }}
+          />
+        )
+      ) : null}
       <div className={styles.powered}>
         <Typography variant="md">Powered by</Typography>
         <div className={styles.icons} aria-label="icons">
